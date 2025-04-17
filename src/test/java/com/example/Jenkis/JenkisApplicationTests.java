d3f4fbc9414fc2a54e343d978f861d4b922e906a
eyamustapha <mustaphaeya922@gmail.com>
2025-04-17 15:59:45 +0200

    Update JenkisApplicationTests.java

--- src/test/java/com/example/Jenkis/JenkisApplicationTests.java
+++ src/test/java/com/example/Jenkis/JenkisApplicationTests.java
@@ -2,17 +2,14 @@ package com.example.Jenkis;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals; 
 
 @SpringBootTest
 class JenkisApplicationTests {
 
 	@Test
 	void contextLoads() {
-	   System.out.println("it is a test class")	;
-	   assertEquals(true,true);
-
-
+		System.out.println("it is a test class");
+		assertEquals(true, true); 
 	}
-
 }
-
