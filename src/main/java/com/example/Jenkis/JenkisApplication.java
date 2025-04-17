8725f89cbe5b49ed03133ec3d33c6bbb66d01447
eyamustapha <mustaphaeya922@gmail.com>
2025-04-17 15:56:23 +0200

    Update JenkisApplication.java

--- src/main/java/com/example/Jenkis/JenkisApplication.java
+++ src/main/java/com/example/Jenkis/JenkisApplication.java
@@ -8,7 +8,6 @@ public class JenkisApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkisApplication.class, args);
-		System.out.println('It is a continuos integration job ')
+		System.out.println("It is a continuous integration job");
 	}
-
 }
